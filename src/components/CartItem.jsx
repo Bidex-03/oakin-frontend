9b5146222469328842eae9dbc467996b6df31a80
Abdullah Qaasim <abdullahqaasim14@gmail.com>
2024-02-13 15:44:24 +0100

    fix(chore): fixed an issue

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -14,7 +14,7 @@ const CartItem = ({ item }) => {
   return (
     <li className="flex flex-col border-y border-stone-200 py-4 md:flex-row">
       <img src={image} alt="Living room" className="h-96 rounded-2xl" />
-      <div className="mt-4 flex grow flex-col items-end justify-start gap-4 md:mt-0 md:flex-row md:items-center md:justify-between md:gap-0 md:px-[4rem]">
+      <div className="mt-4 flex grow flex-col  justify-start gap-4 md:mt-0 md:flex-row items-start md:items-center md:justify-between md:gap-0 md:px-[4rem]">
         <div className="md:basis-[53rem]">
           <h3 className="text-2xl font-medium md:text-4xl">{name}</h3>
           <p className="mt-2 text-2xl md:text-3xl">{description}</p>
